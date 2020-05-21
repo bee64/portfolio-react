85611eea143839ba3a3cc400edb0e8a70c12d31d
Brad-- <brad.smith.1324@gmail.com>
2020-05-20 22:05:43 -0600

    gatsby build & image loader (ft. image)

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    siteMetadata: {
+        title: "Dev Portfoliooo"
+    },
+    // plugins: [
+    //     {
+    //         resolve: 'gatsby-plugin-typescript',
+    //         options: {
+    //             isTSX: true,
+    //             jsxPragma: 'jsx',
+    //             allExtensions: true
+    //         }
+    //     }
+    // ]
+    plugins: [
+        'gatsby-plugin-typescript'
+    ]
+}
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 
 // import profilePicture from "../assets/profile_1.jpg";
 // const profilePicture = require('../assets/profile_1.jpg');
+import profilePicture from '../assets/profile_1.jpg';
 
 export interface IntroProps { colorMode: string }
 
 export const Intro = (props: IntroProps) => <section className={'intro ' + props.colorMode}>
-    <div className="img-frame">
-        <img src={'../assets/profile_1.jpg'} />
-    </div>
     <div className="title-frame">
         <h1>Brad Smith</h1>
         <h2>Software Engineer in Fort Collins, Colorado</h2>
+    </div>
+    <div className="img-frame">
+        <img src={profilePicture} 
+            alt="a picture of me, smiling"/>
     </div>
 </section>
